a33b9a5c8df3cab4bebbdb5e4fedc92ef8b5e349
Felipe Gomes <NoBreakfast1994@proton.me>
2024-06-13 17:37:59 -0300

    Replace Bootstrap with Tailwind CSS

--- app/javascript/entrypoints/application.js
+++ app/javascript/entrypoints/application.js
@@ -1,6 +1,4 @@
 import "@hotwired/turbo-rails";
-import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 console.log("Vite ⚡️ Rails");
 console.log("Read more: https://vite-ruby.netlify.app/guide/rails");
---
+++ tailwind.config.js
@@ -0,0 +1,5 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["app/**/*.html.erb"],
+  plugins: [require("daisyui")],
+};
