25a41c7e65d691b3faaa9b8ae97e1c3e2342e846
Felipe Gomes <NoBreakfast1994@proton.me>
2024-06-16 12:55:21 -0300

    Convert business to singular resource. Implement basic first-time access flow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,22 @@
 # Controller for managing the application.
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, unless: :devise_controller?
+  before_action :check_business_presence, unless: :devise_controller?
 
   private
 
+  def business_controller?
+    controller_name == 'businesses' && action_name.in?(%w[new create])
+  end
+
   def current_business
     @current_business ||= current_user.business if user_signed_in?
+  end
+
+  def check_business_presence
+    return unless current_business.nil? && !business_controller?
+
+    redirect_to new_business_path, alert: t('controllers.application.alerts.create_business_profile')
   end
 
   helper_method :current_business
--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -2,59 +2,62 @@
 
 # Controller for managing businesses.
 class BusinessesController < ApplicationController
+  before_action :redirect_if_business_exists, only: %i[new create]
   before_action :set_business, only: %i[show edit update destroy]
 
-  # GET `/businesses`.
-  def index
-    @businesses = Business.all
-  end
-
-  # GET `/businesses/1`.
+  # GET `/business`.
   def show; end
 
-  # GET `/businesses/new`.
+  # GET `/business`.
   def new
-    @business = Business.new
+    @business = current_user.build_business
   end
 
-  # GET `/businesses/1/edit`.
+  # GET `/business`.
   def edit; end
 
-  # POST `/businesses`.
+  # POST `/business`.
   def create
-    @business = Business.new(business_params)
+    @business = current_user.build_business(business_params)
 
     if @business.save
-      redirect_to @business, notice: t('controllers.businesses.create.success')
+      redirect_to business_path, notice: t('controllers.businesses.create.success')
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT `/businesses/1`.
+  # PATCH/PUT `/business`.
   def update
     if @business.update(business_params)
-      redirect_to @business, notice: t('controllers.businesses.update.success'), status: :see_other
+      redirect_to business_path, notice: t('controllers.businesses.update.success'), status: :see_other
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE `/businesses/1`.
+  # DELETE `/business`.
   def destroy
     @business.destroy!
-    redirect_to businesses_url, notice: t('controllers.businesses.destroy.success'), status: :see_other
+
+    redirect_to new_business_path, notice: t('controllers.businesses.destroy.success'), status: :see_other
   end
 
   private
 
   # Use callbacks to share common setup or constraints between actions.
   def set_business
-    @business = Business.find(params[:id])
+    @business = current_business
   end
 
   # Only allow a list of trusted parameters through.
   def business_params
-    params.require(:business).permit(:name, :user_id)
+    params.require(:business).permit(:name)
+  end
+
+  def redirect_if_business_exists
+    return if current_user.business.blank?
+
+    redirect_to business_path, alert: t('controllers.businesses.alerts.business_exists')
   end
 end
